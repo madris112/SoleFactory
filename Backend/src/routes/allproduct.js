f95f9e7b6c491cb41118e8f6516514a1dc088374
madris112 <madhur.nilu@gmail.com>
2021-05-20 16:44:52 +0530

    allproducts API updates

--- Backend/src/routes/allproduct.js
+++ Backend/src/routes/allproduct.js
@@ -18,6 +18,8 @@ router.post('/createProduct', function (req, res){
                     product.create({
                         Title          : req.body.title,
                         description    : req.body.description,
+                        brand          : req.body.brand,
+                        Quantity       : req.body.Quantity,
                         price          : req.body.price,
                         seller         : req.body.seller,
                         expiryDate     : req.body.expiryDate,
@@ -45,11 +47,67 @@ router.post('/createProduct', function (req, res){
                 }
             } else{
     
-                console.log("already exist! update its value\n" + doc);
-                res.status(201).send({
-                    message: "already exist! update its value\n",
-                    product: doc
-                });
+                try {
+                    product.findOne({
+                        Title : req.body.title,
+                        Seller: req.body.seller,
+                    }).then((doc) => {
+                        if (!doc) {
+
+                                    coinvalue = parseInt(req.body.price);
+                                    coinvalue = coinvalue / 10;
+    
+                
+                                    try {
+                                            product.create({
+                                            Title          : req.body.title,
+                                            description    : req.body.description,
+                                            brand          : req.body.brand,
+                                            Quantity       : req.body.Quantity,
+                                            price          : req.body.price,
+                                            seller         : req.body.seller,
+                                            expiryDate     : req.body.expiryDate,
+                                            DiscountedPrice: req.body.discountedPrice,
+                                            categoryTag    : req.body.categoryTag,
+                                            Rating         : "0",
+                                            NoOfUserRated  : "0",
+                                            Coinvalue      : coinvalue
+        
+                                        }
+                                        ).then((docs) => {
+        
+                                            console.log("product created successfully\n" + docs);
+                                            res.status(200).send({
+                                                message: "product created successfully",
+                                            product: docs
+                                        });
+                        
+                    });
+                } catch (error) {
+                    console.error("creating a new product failure : " + error)
+                    res.status(400).send({
+                        message: "Failed Try Again !"
+                    })
+                }
+                            
+                        } else{
+
+                            console.log("product already exist");
+                            res.status(201).send({
+                                message:"already exist! update its value\n",
+                                product: doc
+                            });
+                            
+                        }
+                    });
+                } catch (error) {
+
+                    res.status(400).send({
+                        message:"something went wrong",
+                    })
+                    
+                }
+                
                 
             }
         });
