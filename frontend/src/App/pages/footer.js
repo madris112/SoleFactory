937fdd5ff140fbda5270fa394bea76685379676c
Tanishqmalu <54908702+Tanishqmalu@users.noreply.github.com>
2021-05-25 13:09:59 +0530

    added footer

---
+++ frontend/src/App/pages/footer.js
@@ -0,0 +1,101 @@
+import React from "react";
+import './footer.css';
+const Foot = () => (
+  <>
+
+<footer class="footer">
+  <div class="footer__addr">
+    <h1 class="footer__logo">Sole Factory</h1>
+
+    <h2>Contact</h2>
+
+    <address>
+      Indore:Raipur:Patna:Shivpuri<br />
+
+      <a class="footer__btn" href="mailto:example@gmail.com">Email Us</a>
+    </address>
+  </div>
+
+  <ul class="footer__nav">
+    <li class="nav__item">
+      <h2 class="nav__title">Madhur Malpani</h2>
+
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Linked In</a>
+        </li>
+
+        <li>
+          <a href="#">Github</a>
+        </li>
+
+        <li>
+          <a href="#">Email</a>
+        </li>
+      </ul>
+    </li>
+    <li class="nav__item">
+      <h2 class="nav__title">Srajan Khandelwal</h2>
+
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Linked In</a>
+        </li>
+
+        <li>
+          <a href="#">Github</a>
+        </li>
+
+        <li>
+          <a href="#">Email</a>
+        </li>
+      </ul>
+    </li>
+    <li class="nav__item">
+      <h2 class="nav__title">Sushant Sinha</h2>
+
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Linked In</a>
+        </li>
+
+        <li>
+          <a href="#">Github</a>
+        </li>
+
+        <li>
+          <a href="#">Email</a>
+        </li>
+      </ul>
+    </li>
+    <li class="nav__item">
+      <h2 class="nav__title">Tanishq Malu</h2>
+
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Linked In</a>
+        </li>
+
+        <li>
+          <a href="#">Github</a>
+        </li>
+
+        <li>
+          <a href="#">Email</a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+
+  <div class="legal">
+    <p>Amazon Hackathon 2.0</p>
+
+    <div class="legal__links">
+      <span>Made with <span class="heart">♥</span> by Bit Predators</span>
+    </div>
+  </div>
+</footer>
+  </>
+);
+
+export default Foot;
